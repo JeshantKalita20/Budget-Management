bfe962ad6723d5c0dcfd8c7ff8a23f78448f18d3
Jeshant Kalita <jeshant.kalita27@gmail.com>
2021-01-21 20:12:45 +0530

    Commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<div class="col-sm-12" style="background-color: black; position: fixed; bottom: 0; width: 100%; height: 50px; margin-bottom: 0px; ">
+				<p style="text-align: center; color: green; ">COPYRIGHT © 2020 JESHANT KALITA</p>
+				<p style="text-align: center; color: green; "> ALL RIGHTS RESERVED </p>
+			</div>
